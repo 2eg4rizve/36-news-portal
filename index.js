c44877bacfad60b4da9d518ca909d4da1d970a5e
2eg4rizve <2eg4rizve.com>
2023-08-31 15:50:04 +0600

    category_name

--- index.js
+++ index.js
@@ -7,13 +7,34 @@
 
 const handleCategory = async () => {
 
-    console.log("hello 1");
+
+    // console.log("hello 1");
     const response = await fetch(
         "https://openapi.programming-hero.com/api/news/categories"
     );
-    const data =await response.json();
+    const data = await response.json();
 
-    console.log(data);
+    const tabContainer = document.getElementById('tab-container');
+
+    data.data.news_category.slice(0,3).forEach((category) => {
+    
+    console.log(category);
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+
+        <a class="tab">${category.category_name}</a>
+
+        `;
+
+        tabContainer.appendChild(div);
+
+    });
+
+
+
+console.log(data.data.news_category);
 }
 
 
