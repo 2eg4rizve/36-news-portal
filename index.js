26b927291790e68ae7365e6d8b264b3fca01b084
2eg4rizve <2eg4rizve.com>
2023-08-31 17:05:16 +0600

    handleLoadNews

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const handleCategory = async () => {
 
     div.innerHTML = `
 
-        <a onclick="handelLoadNews ('${category
+        <a onclick="handleLoadNews ('${category
             .category_id}') " class="tab">${category.category_name}</a>
 
         `;
@@ -38,9 +38,68 @@ const handleCategory = async () => {
 console.log(data.data.news_category);
 }
 
-const handelLoadNews = (categoryId) =>{
+const handleLoadNews = async (categoryId) => {
     console.log(categoryId);
-}
-
+    const response = await fetch(
+      `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+    );
+    const data = await response.json();
+  
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
+  
+    data.data?.forEach((news) => {
+      console.log(news);
+      const div = document.createElement("div");
+      div.innerHTML = `
+          <div class="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src=${news?.image_url}
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+             ${news.title.slice(0, 40)}
+              <div class="badge badge-secondary p-5">${news?.rating?.badge}</div>
+            </h2>
+            <p>
+            ${news.details.slice(0, 50)}
+            </p>
+            <h3> total viws: ${
+              news.total_view ? news.total_view : "no vviews"
+            }</h3>
+            <div class="card-footer flex justify-between mt-8">
+              <div class="flex">
+                <div>
+                  <div class="avatar online">
+                    <div class="w-14 rounded-full">
+                      <img
+                        src=${news.author?.img}
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <h6>${news.author?.name}</h6>
+                  <small>2022-08-24 17:27:34</small>
+                </div>
+              </div>
+              <div class="card-detaild-btn">
+                <button onclick=handleModal('${news._id}')
+                  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900"
+                >
+                  Details
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+          
+          `;
+  
+      cardContainer.appendChild(div);
+    });
+  };
 
 handleCategory();
