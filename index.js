1206a2e4dbe55be41b7fa3af54d1acfb83782d75
2eg4rizve <2eg4rizve.com>
2023-08-31 17:21:49 +0600

    card-container

--- index.js
+++ index.js
@@ -16,16 +16,16 @@ const handleCategory = async () => {
 
     const tabContainer = document.getElementById('tab-container');
 
-    data.data.news_category.slice(0,3).forEach((category) => {
-    
-    console.log(category);
+    data.data.news_category.slice(0, 3).forEach((category) => {
 
-    const div = document.createElement("div");
+        console.log(category);
 
-    div.innerHTML = `
+        const div = document.createElement("div");
+
+        div.innerHTML = `
 
         <a onclick="handleLoadNews ('${category
-            .category_id}') " class="tab">${category.category_name}</a>
+                .category_id}') " class="tab">${category.category_name}</a>
 
         `;
 
@@ -35,23 +35,23 @@ const handleCategory = async () => {
 
 
 
-console.log(data.data.news_category);
+    console.log(data.data.news_category);
 }
 
 const handleLoadNews = async (categoryId) => {
     console.log(categoryId);
     const response = await fetch(
-      `https://openapi.programming-hero.com/api/news/category/${categoryId}`
+        `https://openapi.programming-hero.com/api/news/category/${categoryId}`
     );
     const data = await response.json();
-  
+
     const cardContainer = document.getElementById("card-container");
     cardContainer.innerHTML = "";
-  
+
     data.data?.forEach((news) => {
-      console.log(news);
-      const div = document.createElement("div");
-      div.innerHTML = `
+        console.log(news);
+        const div = document.createElement("div");
+        div.innerHTML = `
           <div class="card w-96 bg-base-100 shadow-xl">
           <figure>
             <img
@@ -66,8 +66,7 @@ const handleLoadNews = async (categoryId) => {
             <p>
             ${news.details.slice(0, 50)}
             </p>
-            <h3> total viws: ${
-              news.total_view ? news.total_view : "no vviews"
+            <h3> total viws: ${news.total_view ? news.total_view : "no views"
             }</h3>
             <div class="card-footer flex justify-between mt-8">
               <div class="flex">
@@ -97,9 +96,10 @@ const handleLoadNews = async (categoryId) => {
         </div>
           
           `;
-  
-      cardContainer.appendChild(div);
+
+        cardContainer.appendChild(div);
     });
-  };
+};
 
 handleCategory();
+handleLoadNews("01");
